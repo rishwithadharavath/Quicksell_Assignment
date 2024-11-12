6876b1374bbe812cbeb1dfea5bcb5052c4747074
Rishwitha <somladharavath911@gmail.com>
2024-11-12 16:28:56 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import Header from "./components/Header";
+import Grid from "./components/Grid";
+import { GET_TICKETS_URL } from "./constants";
+import { loadGrid, mapUsersByUserId } from "./utils";
+import { Ticket, User } from "./interfaces";
+import Loader from "./components/Loader";
+import "./App.css";
 
 function App() {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [userData, setUserData] = useState<Record<string, User>>({});
+  const [gridData, setGridData] = useState<Record<string, Ticket[]>>({});
+  const [grouping, setGrouping] = useState<string>("status");
+  const [ordering, setOrdering] = useState<string>("priority");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadSettings();
+    fetch(GET_TICKETS_URL)
+      .then((resp) => resp.json())
+      .then((res) => {
+        const { tickets, users } = res;
+        setTickets(tickets);
+        setUserData(mapUsersByUserId(users));
+      })
+      .catch((err) => {});
+  }, []);
+
+  useEffect(() => {
+    if (!tickets.length) return;
+    setGridData(loadGrid(tickets, grouping, ordering));
+    setLoading(false);
+  }, [grouping, ordering, tickets]);
+
+  const onSetGrouping = useCallback((value: string) => {
+    setLoading(true);
+    setGrouping(value);
+    saveSettings({ grouping: value });
+  }, []);
+
+  const onSetOrdering = useCallback((value: string) => {
+    setLoading(true);
+    setOrdering(value);
+    saveSettings({ ordering: value });
+  }, []);
+
+  const saveSettings = useCallback((data: Record<string, string>) => {
+    for (let key in data) localStorage.setItem(key, data[key]);
+  }, []);
+
+  const loadSettings = useCallback(() => {
+    setGrouping(localStorage.getItem("grouping") || "status");
+    setOrdering(localStorage.getItem("ordering") || "priority");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        grouping={grouping}
+        setGrouping={onSetGrouping}
+        ordering={ordering}
+        setOrdering={onSetOrdering}
+      />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Grid gridData={gridData} grouping={grouping} userIdToData={userData} />
+      )}
     </div>
   );
 }
